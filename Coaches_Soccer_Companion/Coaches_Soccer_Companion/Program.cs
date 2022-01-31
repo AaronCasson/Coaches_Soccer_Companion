4d61c25573485d96857fc85fb77d402de1e8f16c
Aaron Casson <AaronCasson@gmail.com>
2022-01-31 17:34:35 -0500

    Initial file upload to git.  Contains rough outline of project idea.

---
+++ Coaches_Soccer_Companion/Coaches_Soccer_Companion/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Coaches_Soccer_Companion
+{
+    class project
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+
+        public static void Teams()
+        {
+            string teamName{ AttributeTargets; };
+            int wins;
+            int losses;
+            int ties;
+            int matchesPlayed;
+
+        }
+
+        public static void Positions()
+        {
+            string goalKeeper;
+            string defender;
+            string midfielder;
+            string forwad;
+
+         }
+
+        public static void Players()
+        {
+            string playerFirstName;
+            string playerLastName;
+            string playerDOB;
+        }
+
+        public static void Coaches()
+        {
+            string coachFirstName;
+            string coachLastName;
+            string coachPhoneNumber;
+        }
+
+
+
+        public static void PlayerStats()
+        {
+            double matchesAttended;
+            double timePlayedTotal;
+            double percentOfGamePlayed;
+
+        }
+    }
+}
